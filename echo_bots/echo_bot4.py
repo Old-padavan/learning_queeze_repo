a7834f1969f826c0c6861507af9a91851c40e0d0
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-04-04 15:58:51 +0300

    Создан бот, пересылающий почти все виды сообщений

---
+++ echo_bots/echo_bot4.py
@@ -0,0 +1,35 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+api_token: str = '6030785391:AAFiapS-QAvi1-FAR2tx0o5YEl9WtxtqUGo'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=api_token)
+dp: Dispatcher = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async  def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
