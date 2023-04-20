3916f8684e69821d83edd17f1dc8157a2cbdfcad
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-04-20 16:06:39 +0300

    создан файл с хенлерами, срабатывающими на сообщения 'help' и 'start'

---
+++ modular_echo_bot/handlers/user_handlers.py
@@ -0,0 +1,12 @@
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from lexicon.lexicon import LEXICON_RU
+
+@dp.message(CommandStart())
+async def command_start_process(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+# Этот хэндлер срабатывает на команду /help
+@dp.message(Command(commands='help'))
+async def command_help_process(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
