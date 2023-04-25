e92ab30e81e5d3cd3d665facda3792d3de8a5161
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-04-25 10:14:58 +0300

    Бот с настройкой клавиатуры через ReplyKeyboardBuilder. Автоматическое размещение сначала 6-ти кнопок с параметром width=4, а затем еще 4-х кнопок с параметром width=3

--- exersizes/buttons_bot_2.py
+++ exersizes/buttons_bot_2.py
@@ -12,12 +12,19 @@ dp: Dispatcher = Dispatcher()
 # Инициализируем объект билдера
 kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
-# Создаем список с кнопками (например, 10 кнопок)
-buttons: list[KeyboardButton] = [KeyboardButton(text=f'Кнопка {i + 1}') for i in range(8)]
+# Создаем первый список с кнопками
+buttons_1: list[KeyboardButton] = [KeyboardButton(text=f'Кнопка {i + 1}') for i in range(6)]
+
+## Создаем второй список с кнопками
+buttons_2: list[KeyboardButton] = [KeyboardButton(text=f'Кнопка {i + 7}') for i in range(4)]
 
 # Распаковываем список с кнопками в билдер, указываем, что
-# в одном ряду должно быть 3 кнопки
-kb_builder.row(*buttons, width=3)
+# в одном ряду должно быть 4 кнопки
+kb_builder.row(*buttons_1, width=4)
+
+# Еще раз распаковываем список с кнопками в билдер, указываем, что
+# теперь в одном ряду должно быть 3 кнопки
+kb_builder.row(*buttons_2, width=3)
 
 # Этот хэндлер будет срабатывать на команду "/start"
 # и отправлять в чат клавиатуру
