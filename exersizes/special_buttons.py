09762100726f1f7817ddd74d069c736488202f51
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-04-27 13:59:33 +0300

    Бот с тестированием специальных кнопок. Добавлена возможность выбора между опросом и викториной. Добавлена кнопка перехода в web приложение

--- exersizes/special_buttons.py
+++ exersizes/special_buttons.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType, Message
+from aiogram.types.web_app_info import WebAppInfo
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 API_TOKEN: str = '6030785391:AAFiapS-QAvi1-FAR2tx0o5YEl9WtxtqUGo'
@@ -21,17 +22,26 @@ poll_btn_2: KeyboardButton = KeyboardButton(text='Cоздать опрос',
                                             request_poll=KeyboardButtonPollType(type='regular'))
 quiz_btn: KeyboardButton = KeyboardButton(text='Создать викторину',
                                             request_poll=KeyboardButtonPollType(type='quiz'))
+web_app_btn: KeyboardButton = KeyboardButton(text='Start web app',
+                                             web_app=WebAppInfo(url="https://stepik.org/"))
 
 # Добавляем кнопки в билдер
-kb_builder.row(contact_btn, geo_btn, poll_btn_2, quiz_btn)
+kb_builder.row(contact_btn, geo_btn, poll_btn_2, quiz_btn, web_app_btn)
 
 # Создаем объект клавиатуры
 keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+web_up_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[web_app_btn]],
+                                                           resize_keyboard=True)
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(text='Эксперементируем со специальными кнопками', reply_markup=keyboard)
+    await message.answer(text='Экспериментируем со специальными кнопками', reply_markup=keyboard)
+
+# Этот хэндлер будет срабатывать на команду "/web_app"
+@dp.message(Command(commands='web_app'))
+async def process_web_app_command(message: Message):
+    await message.answer(text='Перейти по ссылке: ', reply_markup=web_up_keyboard)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
