f9a233cfe6aa304faa942ab7078c0034c81f5393
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-05-12 10:01:30 +0300

    Бот с инлайн CallBack кнопками, добавлено изменение сообщения над кнопками при нажатии кнопок

--- inline_buttons/callback_buttons.py
+++ inline_buttons/callback_buttons.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from aiogram.filters import CommandStart, Text
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
@@ -26,6 +26,26 @@ keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[big_butt
 @dp.message(CommandStart())
 async def Process_command_start(message: Message):
     await message.answer(text='Это инлайн-кнопки. Нажми на любую!', reply_markup=keyboard)
+
+# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# с data 'big_button_1_pressed'
+@dp.callback_query(Text(text=['big_button_1_pressed']))
+async def process_button_1_press(callback: CallbackQuery):
+    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 1':
+        await callback.message.edit_text(
+            text='Была нажата БОЛЬШАЯ КНОПКА 1',
+            reply_markup=callback.message.reply_markup)
+    await callback.answer()
+
+# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# с data 'big_button_2_pressed'
+@dp.callback_query(Text(text=['big_button_2_pressed']))
+async def process_button_2press(callback: CallbackQuery):
+    if callback.message.text != 'Была нажата БОЛЬШАЯ КНОПКА 2':
+        await callback.message.edit_text(
+            text='Была нажата БОЛЬШАЯ КНОПКА 2',
+            reply_markup=callback.message.reply_markup)
+    await callback.answer()
 
 if __name__ == '__main__':
     dp.run_polling(bot)
