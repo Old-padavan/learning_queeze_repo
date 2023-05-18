5a5d28ea2bf58c7cf930a66a533de26878a7750f
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-05-18 10:00:05 +0300

    Создан текстовый файл с пользовательскими хендлерами. Исправлена ошибка - добавлена кодировка. Исправлены ошибки

--- BookBot/services/file_handling.py
+++ BookBot/services/file_handling.py
@@ -1,6 +1,8 @@
 BOOK_PATH = 'book/book.txt'
 PAGE_SIZE = 1050
 
+book: dict[int, str] = {}
+
 # Функция, возвращающая строку с текстом страницы и ее размер
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     end_signs = ',.!:;?'
@@ -25,7 +27,7 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
     with open(path, 'r', encoding='utf-8') as file:
-        file.read()
+        text = file.read()
     start, page_number = 0, 1
     while start < len(text):
         page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
